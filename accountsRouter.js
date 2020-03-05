a73088b15da47cad6a16893188deab455970df07
Jesse-Tingle <jtingle0@gmail.com>
2020-03-04 22:00:27 -0500

    GET by ID route for accounts complete

--- accountsRouter.js
+++ accountsRouter.js
@@ -12,21 +12,25 @@ router.get("/", async (req, res, next) => {
 	}
 });
 
-// router.get("/:id", async (req, res, next) => {
-// 	try {
-// 		const accounts = await db.select("*").from("accounts");
-// 		res.json(accounts);
-// 	} catch (error) {
-// 		next(error);
-// 	}
-// });
+router.get("/:id", async (req, res, next) => {
+	try {
+		const accounts = await db
+			.select("*")
+			.from("accounts")
+			.where("id", req.params.id)
+			.limit(1);
+		res.json(accounts);
+	} catch (error) {
+		next(error);
+	}
+});
 
-// router.post("/", async (req, res, next) => {
-// 	try {
-// 	} catch (error) {
-// 		next(error);
-// 	}
-// });
+router.post("/", async (req, res, next) => {
+	try {
+	} catch (error) {
+		next(error);
+	}
+});
 
 // router.put("/:id", async (req, res, next) => {
 // 	try {
