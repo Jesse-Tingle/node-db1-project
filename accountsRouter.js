c4af56a101d0447d2640759ee7daf3677d360863
Jesse-Tingle <jtingle0@gmail.com>
2020-03-07 23:17:22 -0500

    add UPDATE route

--- accountsRouter.js
+++ accountsRouter.js
@@ -27,17 +27,37 @@ router.get("/:id", async (req, res, next) => {
 
 router.post("/", async (req, res, next) => {
 	try {
+		const payload = {
+			name: req.body.name,
+			budget: req.body.budget
+		};
+		const [id] = await db.insert(payload).into("accounts");
+		const newAccount = await db("accounts")
+			.where("id", id)
+			.first();
+		res.json(newAccount);
 	} catch (error) {
 		next(error);
 	}
 });
 
-// router.put("/:id", async (req, res, next) => {
-// 	try {
-// 	} catch (error) {
-// 		next(error);
-// 	}
-// });
+router.put("/:id", async (req, res, next) => {
+	try {
+		const payload = {
+			name: req.body.name,
+			budget: req.body.budget
+		};
+		await db("accounts")
+			.where("id", req.params.id)
+			.update(payload);
+		const updateAccount = await db("accounts")
+			.where("id", req.params.id)
+			.first();
+		res.status(updateAccount);
+	} catch (error) {
+		next(error);
+	}
+});
 
 // router.delete("/:id", async (req, res, next) => {
 // 	try {
